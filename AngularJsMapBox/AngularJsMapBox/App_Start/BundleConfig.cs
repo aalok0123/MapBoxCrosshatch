0780298c0796fd4d1837eab6e1480150c1900e03
Aalok Dahayat <aalok0123@gmail.com>
2017-07-26 18:37:35 +0530

    first commit

---
+++ AngularJsMapBox/AngularJsMapBox/App_Start/BundleConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace AngularJsMapBox
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/custom").Include(
+                "~/Scripts/spa/app.js",
+"~/Scripts/spa/mapBox/mapBoxCtrl.js"
+                          ));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                       "~/Scripts/angular.js",
+                       "~/Scripts/angular-ui-router.js",
+                        "~/Scripts/angular-animate.js"));
+
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap.min.css"
+                      ));
+
+        }
+    }
+}
